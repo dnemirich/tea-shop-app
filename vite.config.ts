badf09db8503c4c8939e4b282f10c5dadd4904af
dnemirich <dnemirich@gmail.com>
2025-04-30 17:16:58 +0200

    chore: update vite.config.ts to include prod and dev modes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    build: {
+      minify: mode === 'production' ? 'esbuild' : false,
+      sourcemap: mode !== 'production',
+    },
+    server: {
+      open: mode === 'development',
+    },
+  };
 });
