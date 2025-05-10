b0e71429d91f927d610503178fd06c7e80d97ee9
alinidi <alinareyz@gmail.com>
2025-05-10 09:58:59 +0500

    chore: fix stylelint errors and add stylelint hook to husky

--- src/features/LoginPage/LoginPage.tsx
+++ src/features/LoginPage/LoginPage.tsx
@@ -93,61 +93,65 @@ function LoginPage() {
   const containsCyrillic = (text: string) => /[а-яА-ЯёЁ]/.test(text);
 
   return (
-    <div className={cl.login_page_wrapper}>
+    <div className={cl['login-page-wrapper']}>
       <form onSubmit={handleSubmit(onSubmit)} className={cl.form} noValidate>
-        <div className={cl.form_title}>
+        <div className={cl['form-title']}>
           <h2>Already a customer?</h2>
           <p>Welcome back! Sign in for faster checkout.</p>
         </div>
-        <div className={cl.label_wrapper}>
-          <label className={cl.form_label}>
-            <img className={cl.img_icon} src={mailIcon} alt="mail-img" />
+        <div className={cl['label-wrapper']}>
+          <label className={cl['form-label']}>
+            <img className={cl['img-icon']} src={mailIcon} alt="mail-img" />
             <input
               {...register('email')}
-              className={cl.form_input}
+              className={cl['form-input']}
               type="email"
               placeholder="Email Address"
             />
           </label>
-          {errors.email && <p className={cl.input_error}>{`${errors.email.message}`}</p>}
+          {errors.email && <p className={cl['input-error']}>{`${errors.email.message}`}</p>}
         </div>
-        <div className={cl.label_wrapper}>
-          <label className={cl.form_label}>
-            <img className={cl.img_icon} src={passwordIcon} alt="mail-img" />
+        <div className={cl['label-wrapper']}>
+          <label className={cl['form-label']}>
+            <img className={cl['img-icon']} src={passwordIcon} alt="mail-img" />
             <input
               {...register('password')}
-              className={cl.form_input}
+              className={cl['form-input']}
               type={showPassword ? 'text' : 'password'}
               placeholder="Enter your password"
               onChange={(e) => {
-                const char = e.target.value;
-                setHasCyrillic(containsCyrillic(char));
+                const value = e.target.value;
+                setHasCyrillic(containsCyrillic(value));
               }}
             />
-            <button type="button" onClick={togglePasswordVisibility} className={cl.toggle_button}>
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={cl['toggle-button']}
+            >
               {showPassword ? 'Hide' : 'Show'}
             </button>
           </label>
-          {errors.password && <p className={cl.input_error}>{`${errors.password.message}`}</p>}
+          {errors.password && <p className={cl['input-error']}>{`${errors.password.message}`}</p>}
           {hasCyrillic && (
-            <p className={cl.input_warning}>Please make sure your keyboard is set to English</p>
+            <p className={cl['input-warning']}>Please make sure your keyboard is set to English</p>
           )}
         </div>
-        <div className={cl.form_options}>
-          <label className={cl.form_checkbox_label} htmlFor="checkbox-Input">
+        <div className={cl['form-options']}>
+          <label className={cl['form-checkbox-label']} htmlFor="checkbox-Input">
             <input
               type="checkbox"
               id="checkbox_input"
-              className={cl.form_checkbox_input}
+              className={cl['form-checkbox-input']}
               {...register('rememberMe')}
             />
             <span>Please remember me</span>
           </label>
-          <a className={cl.form_link} href="/#">
+          <a className={cl['form-link']} href="/#">
             Forgot password?
           </a>
         </div>
-        <button disabled={isSubmitting} type="submit" className={cl.signIn_button}>
+        <button disabled={isSubmitting} type="submit" className={cl['sign-in-button']}>
           SIGN IN
         </button>
       </form>
