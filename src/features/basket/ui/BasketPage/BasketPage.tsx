a861edb305089e8c5c320711a7c4472d723467ed
alinidi <alinareyz@gmail.com>
2025-06-15 20:02:54 +0500

    feat: clean up backet page and implement button navigation

--- src/features/basket/ui/BasketPage/BasketPage.tsx
+++ src/features/basket/ui/BasketPage/BasketPage.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
-import { ProgressPages } from './ProgressPages/ProgressPages';
-import { Stepper } from './Stepper/Stepper';
+import { MyBagPage } from './MyBagPage/MyBagPage';
 
 export const BasketPage = () => {
-  const [selectedOption, setSelectedOption] = useState<
-    'MyBagPage' | 'DeliveryPage' | 'PaymentPage' | null
-  >(null);
-
   return (
     <div>
-      <Stepper />
-      <ProgressPages selectedOption={selectedOption} />
+      <MyBagPage />
     </div>
   );
 };
--- src/features/basket/ui/BasketPage/MyBagPage/ItemsContainer/ItemsContainer.tsx
+++ src/features/basket/ui/BasketPage/MyBagPage/ItemsContainer/ItemsContainer.tsx
@@ -3,8 +3,15 @@ import { ItemCard } from '../ItemCard/ItemCard';
 import s from './itemsContainer.module.scss';
 import { CostInfo } from '../../CostInfo/CostInfo';
 import { Line } from '../../Line/Line';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '@/common/config/routes';
 
 export const ItemsContainer = () => {
+  const navigate = useNavigate();
+  const handleBackShopping = () => {
+    navigate(ROUTES.SHOP);
+  };
+
   return (
     <div className={s.itemsWrapper}>
       <ItemCard />
@@ -12,7 +19,9 @@ export const ItemsContainer = () => {
       <ItemCard />
       <Line />
       <CostInfo text="Subtotal" />
-      <Button className={s.button}>back to shopping</Button>
+      <Button className={s.button} onClick={handleBackShopping}>
+        back to shopping
+      </Button>
     </div>
   );
 };
