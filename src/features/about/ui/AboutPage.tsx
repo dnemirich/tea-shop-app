dbc401d471f5139484fdac7c0557f73b085bfd92
dnemirich <dnemirich@gmail.com>
2025-06-16 16:10:23 +0200

    chore: appply linter

--- src/features/about/ui/AboutPage.tsx
+++ src/features/about/ui/AboutPage.tsx
@@ -39,11 +39,15 @@ export const AboutPage = () => {
         <div className={s.aboutText}>
           <p>
             This tea store project was created as a final assignment for the JavaScript/Front-end
-            2024Q4 course at Rolling Scope School <a href={'https://rs.school'}><img src={logo} alt={'Rolling scopes school logo'} className={s.logo} /></a>. The main task was to create an online store based
-            on the commercetools platform. Our team built the store using a lightweight tech stack:
-            React, Zustand, React Router, Swiper, React Hook Form, and Zod. With no final UI
-            mockups, we relied on open communication, shared ideas, and each teammate’s creative
-            input to shape the product collaboratively.
+            2024Q4 course at Rolling Scope School{' '}
+            <a href={'https://rs.school'}>
+              <img src={logo} alt={'Rolling scopes school logo'} className={s.logo} />
+            </a>
+            . The main task was to create an online store based on the commercetools platform. Our
+            team built the store using a lightweight tech stack: React, Zustand, React Router,
+            Swiper, React Hook Form, and Zod. With no final UI mockups, we relied on open
+            communication, shared ideas, and each teammate’s creative input to shape the product
+            collaboratively.
           </p>
         </div>
       </section>
